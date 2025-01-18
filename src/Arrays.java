be3d28690ce544def72c1533a2abbfabdca5d6c1
siri8p <siri8p@gmail.com>
2025-01-17 22:17:33 -0800

    array base code

---
+++ src/Arrays.java
@@ -0,0 +1,25 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int nums[] = {3, 4, 5, 8, 2, 1};
+        int target =16;
+        int result = linearSearch(nums, target);
+        if (result != -1)
+            System.out.println("element found at index  " + result);
+        else
+            System.out.println("element is not found");
+
+
+    }
+    public static int linearSearch(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                return i;
+        }
+           return -1;
+    }
+}
+
+
+
+
