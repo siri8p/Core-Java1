d99bde374e5b2c5b9a29b72cac2f0bfdf1b19190
siri8p <siri8p@gmail.com>
2025-01-19 15:46:48 -0800

    inserion of array elements1

--- src/Insert.java
+++ src/Insert.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 public class Insert {
     public static void main(String[] args) {
-        int a[] ={5,7,6,9};
-         int element=89;
+        //int a[]= {5,7,4,89,3};
+        // int element =56;
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter the array size");
+             int size =  s.nextInt();
+             int a[] = new int[size];
+        System.out.println("enter the elements");
+             for(int i=0;i<a.length;i++){
+                   a[i]         = s.nextInt();
+             }
+
          int b[] = new int[a.length+1];
-           b[0] =element;
-          for(int i=0; i<a.length; i++){
-               b[i+1] =a[i];
+        System.out.println("enter the new element");
+
+        // b[0]= element;
+               int element=s.nextInt();
+        b[0]= element;
+         for(int i=0;i<a.length; i++){
+             b[i+1]   =      a[i];
+
           }
-           for(int i=0; i<b.length;i++){
-               System.out.println(b[i]);
-           }
+        for(int i=0;i<b.length; i++){
+           System.out.println(b[i]);
+       }
 
 
 
